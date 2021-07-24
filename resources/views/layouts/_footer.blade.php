7eded68afea3450bfab1cc5eb4ed1f7f62fd43de
gaoyanshuo <gaoyanshuo9388@yahoo.com>
2021-07-24 21:38:19 +0900

    切割头部和尾部子视图include

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+  <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
+  <a href="https://laravel.com" target=_blank>
+    刻意练习，每日精进，终身学习
+  </a>
+
+  <div class="float-right">
+    <a href="/about" >关于</a>
+  </div>
+</footer>
